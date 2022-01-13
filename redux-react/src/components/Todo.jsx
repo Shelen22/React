84b0009a2a87ce442b3e301b3391b6030e7b470e
Shailendra Singh <shailendrauday5@gmail.com>
2022-01-13 18:47:26 +0530

    f

--- redux-react/src/components/Todo.jsx
+++ redux-react/src/components/Todo.jsx
@@ -9,6 +9,7 @@ export const Todos = () => {
   const dispatch = useDispatch()
   const handleinput = (e) => {
     setText(e.target.value);
+    
   };
   const handleTodo = () =>{
       dispatch(addTodoLoading())
@@ -20,18 +21,38 @@ export const Todos = () => {
      .then((d) => d.json())
      .then((res) =>{
          dispatch(addTodoSuccess(res))
+         setText("")
      })
      .catch((err) =>{
          dispatch(addTodoError(err))
      })
+    
   }
+  const handleDelete = () =>{
+    // dispatch(addTodoLoading())
+   fetch("http://localhost:3006/todos/id", {
+       method: "DELETE",
+       body: JSON.stringify({status:false, title:text}),
+       headers: {'Content-Type': 'application/json'}
+   })
+   .then((d) => d.json())
+   .then((res) =>{
+       dispatch(addTodoSuccess(res))
+       setText("")
+   })
+   .catch((err) =>{
+       dispatch(addTodoError(err))
+   })
+  
+}
   return (
     <div>
       <input value ={text} type="text" placeholder="Enter Todo" onChange={handleinput} />
       <button onClick={handleTodo}>Add Todo</button>
       {todos.map((e)=>(
-          <div>{e.title}</div>
+          <div>{e.title}{e.status} </div>    
       ))}
+      <span onClick={handleDelete} >Delete</span>
     </div>
   );
 };
